1ec3959c30860d51c081305d36507614d04cf19d
amirhossein1414 <amirhosseinrezaei14@gmail.com>
2019-10-01 00:18:50 +0330

    asd

--- Blog/src/app/app.module.ts
+++ Blog/src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainMenuComponent } from './main-menu/main-menu.component';
+import { SpaceComponent } from './space/space.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    MainMenuComponent,
+    AppComponent,
+    SpaceComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  entryComponents: [MainMenuComponent],
+  bootstrap: [MainMenuComponent]
 })
 export class AppModule { }
