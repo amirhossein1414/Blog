a2987fa8c87dbc59e40e5a83b124e11c92014e22
amirhossein1414 <amirhosseinrezaei14@gmail.com>
2020-09-03 23:41:34 +0430

    moving-font-awesome

--- Blog/src/app/app.module.ts
+++ Blog/src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainMenuComponent } from './main-menu/main-menu.component';
 import { SpaceComponent } from './space/space.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
 @NgModule({
   declarations: [
@@ -19,6 +19,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     AppRoutingModule,
     FormsModule,
     BrowserAnimationsModule,
+    AngularFontAwesomeModule
   ],
   providers: [],
   entryComponents: [AppComponent],
